e191a70360f89c001b2d075c65b4ecb3884c8d24
Wizard-Mayank <30517csaiml@gmail.com>
2025-11-16 16:33:50 +0530

    Feat: Verify and test basic JNI bridge

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_androidopencvproject_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello Mayank! :)";
     return env->NewStringUTF(hello.c_str());
 }
